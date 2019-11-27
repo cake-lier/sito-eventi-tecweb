9034ce5af92dcd86c7032ef148c01ffdbbdb2735
Giorgia Rondinini <giorgia.rondinini@gmail.com>
2019-11-27 17:02:45 +0100

    Add safe passwords management functions

--- database/database.php
+++ database/database.php
@@ -12,10 +12,12 @@ class DatabaseHelper{
 
     // TODO: how password should be managed? Look on iol
     private function insertUser($email, $password, $profilePhoto, $type) {
-        $query = "INSERT INTO users(email, password, profilePhoto, type) 
-                  VALUES (?, ?, ?, ?)";
+        $query = "INSERT INTO users(email, password, profilePhoto, type, salt) 
+                  VALUES (?, ?, ?, ?, ?)";
+        $salt = hash('sha512', uniqid(mt_rand(1, mt_getrandmax()), true)); // TODO: check if this is ok
+        $password = hashPassword($password, $salt);
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param("ssbi", $email, $password, $profilePhoto, $type);
+        $stmt->bind_param("ssbis", $email, $password, $profilePhoto, $type, $salt);
         $stmt->execute();
         if ($stmt->affected_rows === -1 || $stmt->affected_rows === 0) {
             $result = false;
@@ -61,6 +63,28 @@ class DatabaseHelper{
             $stmt->close();
             return $result;
         }
+    }
+
+    public function checkLogin($email, $plainPassword) {
+        $query = "SELECT email, password, salt
+                  FROM users
+                  WHERE email = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($userEmail, $db_password, $salt); // recupera il risultato della query e lo memorizza nelle relative variabili.
+        $stmt->fetch();
+        if ($stmt->num_rows == 1) {
+            if ($db_password == hashPassword($plainPassword, $salt)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function hashPassword($password, $salt) {
+        return hash('sha512', $password.$salt);
     }
 }
 ?>
