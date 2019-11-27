78674baacddfb85a1f727c66fe82abefd7c646a3
Giorgia Rondinini <giorgia.rondinini@gmail.com>
2019-11-27 21:52:43 +0100

    Add some queries

--- database/database.php
+++ database/database.php
@@ -10,7 +10,7 @@ class DatabaseHelper{
         }
     }
 
-    // TODO: how password should be managed? Look on iol
+    // TODO: where is type definition?
     private function insertUser($email, $password, $profilePhoto, $type) {
         $query = "INSERT INTO users(email, password, profilePhoto, type, salt) 
                   VALUES (?, ?, ?, ?, ?)";
@@ -72,19 +72,205 @@ class DatabaseHelper{
         $stmt = $this->db->prepare($query);
         $stmt->bind_param("s", $email);
         $stmt->execute();
-        $stmt->store_result();
         $stmt->bind_result($userEmail, $db_password, $salt); // recupera il risultato della query e lo memorizza nelle relative variabili.
         $stmt->fetch();
+        $result = false;
         if ($stmt->num_rows == 1) {
             if ($db_password == hashPassword($plainPassword, $salt)) {
-                return true;
+                $result = true;
             }
         }
-        return false;
+        $stmt->close();
+        return $result;
+    }
+    
+    public function changePassword($email, $oldPassword, $newPassword) {
+        if (checkLogin($email, $oldPassword) && isUserLoggedIn($email)) {
+            $query = "SELECT email, salt
+                      FROM users
+                      WHERE email = ?";
+            $stmt = $this->db->prepare($query);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $stmt->bind_result($userEmail, $salt);
+            if ($stmt->fetch()) {
+                // should be just one result
+                $query2 = "UPDATE users
+                           SET password = ?
+                           WHERE email = ?";
+                $stmt2 = $this->db->prepare($query2);
+                $stmt2->bind_param("ss", hashPassword($newPassword, $salt), $email);
+                $stmt2->execute();
+                if ($stmt2->affected_rows === -1 || $stmt2->affected_rows === 0) {
+                    $result = false;
+                } else {
+                    $result = true;
+                }
+                $stmt->close();
+                $stmt2->close();
+                return $result;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public function changeProfilePhoto($email, $photo) {
+        if (isUserLoggedIn($email)) {
+            $query = "UPDATE users
+                      SET profilePhoto = ?
+                      WHERE email = ?";
+            $stmt = $this->db->prepare($query);
+            $stmt->bind_param("bs", $photo, $email);
+            $stmt->execute();
+            if ($stmt2->affected_rows === -1 || $stmt2->affected_rows === 0) {
+                $result = false;
+            } else {
+                $result = true;
+            }
+            $stmt->close();
+            return $result;
+        } else {
+            return false;
+        }
+    }
+
+    public function getUserShortProfile($email) {
+        if (isPromoter($email)) {
+            return getShortPromoterProfile($email);
+        } else if (isCustomer($email)) {
+            if (isPromoter($_SESSION["email"]) || isAdmin($_SESSION["email"]) || isUserLoggedIn($email)) { // TODO: check this
+                return getShortCustomerProfile($email);
+            }
+        } else {
+            return getAdminProfile($email);
+        }
+    }
+
+    public function getUserLongProfile($email) {
+        if (isUserLoggedIn($email)) {
+            if (isPromoter($email)) {
+                return getLongPromoterProfile($email);
+            } else if (isCustomer($email)) {
+                return getShortCustomerProfile($email);
+            } else {
+                getAdminProfile($email);
+            }
+        }
+    }
+
+    private function getShortCustomerProfile($email) {
+        $query = "SELECT username, name, surname, birthDate, birthplace, profilePhoto
+                  FROM users u, customers c 
+                  WHERE u.email = ? 
+                      AND u.email = c.email";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    private function getLongCustomerProfile($email) {
+        $query = "SELECT username, name, surname, birthDate, birthplace, profilePhoto, currentAddress, billingAddress, telephone, email
+                  FROM users u, customers c 
+                  WHERE u.email = ? 
+                      AND u.email = c.email";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    private function getShortPromoterProfile($email) {
+        $query = "SELECT email, profilePhoto, organizationName, website
+                  FROM users u, promoters p 
+                  WHERE u.email = ? 
+                      AND u.email = p.email";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    private function getLongPromoterProfile($email) {
+        $query = "SELECT email, profilePhoto, organizationName, website, VATid
+                  FROM users u, promoter p 
+                  WHERE u.email = ? 
+                      AND u.email = p.email";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    private function getAdminProfile($email) {
+        $query = "SELECT email, profilePhoto
+                  FROM users u
+                  WHERE u.email = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
 
     private function hashPassword($password, $salt) {
         return hash('sha512', $password.$salt);
+    }
+
+    private function isUserType($email, $type) {
+        $query = "SELECT *
+                  FROM users
+                  WHERE email = ?
+                    AND type = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("si", $email, $type);
+        $stmt->execute();
+        return ($stmt->fetch() != null);
+    }
+
+    private function isPromoter($email) {
+        // TODO: change to use type
+        /* return isUserType($email, TYPE); */
+        $query = "SELECT *
+                  FROM promoters
+                  WHERE email = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        return ($stmt->fetch() != null); // if there is no promoter with the given email returns false
+    }
+
+    private function isCustomer($email) {
+        // TODO: change to use type
+        /* return isUserType($email, TYPE); */
+        $query = "SELECT *
+                  FROM customers
+                  WHERE email = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        return ($stmt->fetch() != null); // if there is no promoter with the given email returns false
+    }
+
+    private function isAdmin($email) {
+        // TODO: change to use type
+        /* return isUserType($email, TYPE); */
+        $query = "SELECT *
+                  FROM administrators
+                  WHERE email = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        return ($stmt->fetch() != null); // if there is no promoter with the given email returns false
+    }
+    
+    private function isUserLoggedIn($email) {
+        return $_SESSION["email"] == $email;
     }
 }
 ?>
