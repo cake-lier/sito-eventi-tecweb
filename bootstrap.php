ddd706279a8a36c309bbfcad75501d727d7ea4e4
cake_lier <matteo.castellucci14@hotmail.it>
2019-12-01 22:18:57 +0100

    Add initial PHP bootstrap file

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+define("SERVER_NAME", "localhost");
+define("USER_NAME", "seatheat");
+define("PASSWORD", "");
+define("DATABASE", "my_seatheat");
+
+$db = new DatabaseHelper(SERVER_NAME, USER_NAME, PASSWORD, DATABASE);
+
+?> 
