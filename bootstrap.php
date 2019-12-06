3981c683d066e1d2be3e030cf5661f0553436ceb
cake_lier <matteo.castellucci14@hotmail.it>
2019-12-06 10:39:13 +0100

    Add sessions to bootstrap file

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,13 @@
 <?php
+session_start();
+
+require_once("./database/DatabaseHelper.php")
+
 define("SERVER_NAME", "localhost");
 define("USER_NAME", "seatheat");
 define("PASSWORD", "");
 define("DATABASE", "my_seatheat");
 
-$db = new DatabaseHelper(SERVER_NAME, USER_NAME, PASSWORD, DATABASE);
+$dbh = new DatabaseHelper(SERVER_NAME, USER_NAME, PASSWORD, DATABASE);
 
 ?> 
