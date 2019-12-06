866b9de09833c364bcb4651ce2fc90c4655741b4
cake_lier <matteo.castellucci14@hotmail.it>
2019-12-06 19:16:01 +0100

    Generate creation script from reliable source

--- db_tecweb.sql
+++ db_tecweb.sql
@@ -1,155 +1,107 @@
-create database my_seatheat;
-use my_seatheat;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-create table eventCategories (
-    id int not null auto_increment,
-    name varchar(30) not null,
-    constraint ID_EVENT_CATEGORY primary key (id)
-);
+CREATE DATABASE IF NOT EXISTS `my_seatheat` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_cs;
+USE `my_seatheat`;
 
-create table seatCategories (
-    eventId int not null,
-    id int not null auto_increment,
-    name varchar(30) not null,
-    price decimal(13,2) not null,
-    seats int not null,
-    constraint ID_SEAT_CATEGORY_ID primary key (eventId, id)
-);
+CREATE TABLE IF NOT EXISTS `administrators` (
+  `email` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
-create table events (
-    id int not null auto_increment,
-    name varchar(30) not null,
-    place varchar(30) not null,
-    dateTime datetime not null,
-    description mediumtext not null,
-    site varchar(30),
-    promoterEmail varchar(30),
-    constraint ID_EVENT primary key (id)
-);
+CREATE TABLE IF NOT EXISTS `carts` (
+  `eventId` int(11) NOT NULL,
+  `seatId` int(11) NOT NULL,
+  `customerEmail` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `amount` int(11) NOT NULL,
+  PRIMARY KEY (`eventId`,`seatId`,`customerEmail`),
+  KEY `FK_CUSTOMER` (`customerEmail`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
-create table notifications (
-    id int not null auto_increment,
-    message mediumtext not null,
-    constraint ID_NOTIFICATION primary key (id)
-);
+CREATE TABLE IF NOT EXISTS `customers` (
+  `email` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `username` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `name` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `surname` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `birthDate` date NOT NULL,
+  `birthplace` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `currentAddress` varchar(30) COLLATE latin1_general_cs DEFAULT NULL,
+  `billingAddress` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `telephone` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
-create table usersNotifications (
-    notificationId int not null,
-    email varchar(30) not null,
-    dateTime datetime not null,
-    visualized boolean not null,
-    constraint ID_USER_NOTIFICATION primary key (notificationId, email, dateTime)
-);
+CREATE TABLE IF NOT EXISTS `eventCategories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs AUTO_INCREMENT=2 ;
 
-create table purchases (
-    eventId int not null,
-    seatId int not null,
-    customerEmail varchar(30) not null,
-    amount int not null,
-    constraint ID_PURCHASE primary key (eventId, seatId, customerEmail)
-);
+CREATE TABLE IF NOT EXISTS `events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `place` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `dateTime` datetime NOT NULL,
+  `description` mediumtext COLLATE latin1_general_cs NOT NULL,
+  `site` varchar(30) COLLATE latin1_general_cs DEFAULT NULL,
+  `promoterEmail` varchar(30) COLLATE latin1_general_cs DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_PROMOTER` (`promoterEmail`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs AUTO_INCREMENT=1 ;
 
-create table carts (
-    eventId int not null,
-    seatId int not null,
-    customerEmail varchar(30) not null,
-    amount int not null,
-    constraint ID_CART primary key (eventId, seatId, customerEmail)
-);
+CREATE TABLE IF NOT EXISTS `eventsToCategories` (
+  `categoryId` int(11) NOT NULL,
+  `eventId` int(11) NOT NULL,
+  PRIMARY KEY (`categoryId`,`eventId`),
+  KEY `FK_EVENT` (`eventId`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
-create table eventsToCategories (
-    categoryId int not null,
-    eventId int not null,
-    constraint ID_ASSOCIATION primary key (categoryId, eventId)
-);
+CREATE TABLE IF NOT EXISTS `notifications` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` mediumtext COLLATE latin1_general_cs NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs AUTO_INCREMENT=1 ;
 
-create table users (
-    email varchar(30) not null,
-    password varchar(255) not null,
-    profilePhoto mediumblob not null,
-    type enum('c', 'p', 'a') not null,
-    constraint ID_USER primary key (email)
-);
+CREATE TABLE IF NOT EXISTS `promoters` (
+  `email` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `organizationName` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `VATid` char(11) NOT NULL,
+  `website` varchar(30) COLLATE latin1_general_cs DEFAULT NULL,
+  PRIMARY KEY (`email`),
+  UNIQUE KEY `ID_PROMOTER` (`VATid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
-create table administrators (
-    email varchar(30) not null,
-    constraint FK_IS_ADMINISTRATOR_ID primary key (email)
-);
+CREATE TABLE IF NOT EXISTS `purchases` (
+  `eventId` int(11) NOT NULL,
+  `seatId` int(11) NOT NULL,
+  `customerEmail` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `amount` int(11) NOT NULL,
+  PRIMARY KEY (`eventId`,`seatId`,`customerEmail`),
+  KEY `FK_CUSTOMER` (`customerEmail`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
-create table customers (
-    email varchar(30) not null,
-    username varchar(30) not null,
-    name varchar(30) not null,
-    surname varchar(30) not null,
-    birthDate date not null,
-    birthplace varchar(30) not null,
-    currentAddress varchar(30),
-    billingAddress varchar(30) not null,
-    telephone numeric(11),
-    constraint FK_IS_CUSTOMER_ID primary key (email)
-);
+CREATE TABLE IF NOT EXISTS `seatCategories` (
+  `eventId` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `price` decimal(13,2) NOT NULL,
+  `seats` int(11) NOT NULL,
+  PRIMARY KEY (`eventId`,`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs AUTO_INCREMENT=1;
 
-create table promoters (
-    email varchar(30) not null,
-    organizationName varchar(30) not null,
-    VATid numeric(11) not null,
-    website varchar(30),
-    constraint ID_PROMOTER unique (VATid),
-    constraint FK_IS_PROMOTER_ID primary key (email)
-);
+CREATE TABLE IF NOT EXISTS `users` (
+  `email` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `password` varchar(255) COLLATE latin1_general_cs NOT NULL,
+  `profilePhoto` mediumblob NOT NULL,
+  `type` enum('c','p','a') COLLATE latin1_general_cs NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
-alter table seatCategories add constraint FK_OFFER
-     foreign key (eventId)
-     references eventId (id);
-
--- alter table events add constraint ID_EVENT_CHK
---     check(exists(select * from seatCategories
---                  where seatCategories.eventId = id)); 
-
-alter table events add constraint FK_PROMOTER
-    foreign key (promoterEmail)
-    references promoters (email);
-
-alter table usersNotifications add constraint FK_NOTIFICATION
-    foreign key (notificationId)
-    references notifications (id);
-
-alter table usersNotifications add constraint FK_USER
-    foreign key (email)
-    references users (email);
-
-alter table purchases add constraint FK_EVENT
-     foreign key (eventId, seatId)
-     references seatCategories (eventId, id);
-
-alter table purchases add constraint FK_CUSTOMER
-     foreign key (customerEmail)
-     references customers (email);
-
-alter table carts add constraint FK_EVENT
-     foreign key (eventId, seatId)
-     references seatCategories (eventId, id);
-
-alter table carts add constraint FK_CUSTOMER
-     foreign key (customerEmail)
-     references customers (email);
-
-alter table eventsToCategories add constraint FK_CATEGORY
-    foreign key (categoryId)
-    references eventCategories (id);
-
-alter table eventsToCategories add constraint FK_EVENT
-     foreign key (eventId)
-     references events (id);
-
-alter table administrators add constraint FK_IS_ADMINISTRATOR_FK
-    foreign key (email)
-    references users (email);
-
-alter table customers add constraint FK_IS_CUSTOMER_FK
-    foreign key (email)
-    references users (email);
-
-alter table promoters add constraint FK_IS_PROMOTER_FK
-    foreign key (email)
-    references users (email);
+CREATE TABLE IF NOT EXISTS `usersNotifications` (
+  `notificationId` int(11) NOT NULL,
+  `email` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `dateTime` datetime NOT NULL,
+  `visualized` tinyint(1) NOT NULL,
+  PRIMARY KEY (`notificationId`,`email`,`dateTime`),
+  KEY `FK_USER` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
