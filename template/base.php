b6b699193231474ce3674cdba5d1c0c969a90c8a
Giorgia Rondinini <giorgia.rondinini@gmail.com>
2019-12-08 22:11:34 +0100

    Change images color, add basic css

--- template/base.php
+++ template/base.php
@@ -17,7 +17,8 @@
 </head>
 <body>
     <header>
-        <a href="index.php"><img src="<?php echo IMG_DIR; ?>home-icon.png" alt="home"/><img src="<?php echo IMG_DIR; ?>SeatHeat.png" alt="SeatHeat logo"/></a><!-- TODO: il link -->
+        <a href="index.php"><img id="home" src="<?php echo IMG_DIR; ?>home-icon.png" alt="home"/><img id="logo" src="<?php echo IMG_DIR; ?>SeatHeat.png" alt="SeatHeat logo"/></a>
+        <img id="menu_icon" class="icon" src="<?php echo IMG_DIR; ?>menu.png" alt="menu" />
         <nav>
             <?php if (isset($_SESSION["email"])) {
                     $link_text = "./user_area.php";
@@ -28,13 +29,12 @@
                     $link_image = IMG_DIR."/login.png"; 
                     $link_alt = "Login";
             }?>
-            <!-- TODO: put text in links for mobile version -->
-            <a href="<?php echo $link_text; ?>"><img src="<?php echo $link_image; ?>" alt="<?php echo $link_alt; ?>"/><p class="menu_links"><?php echo $link_alt; ?></p></a>
-            <a href="./cart.php"><img src="<?php echo IMG_DIR; ?>cart.png" alt="carrello"/><p class="menu_links">Carrello</p></a>
+            <a href="<?php echo $link_text; ?>"><img class="icon" src="<?php echo $link_image; ?>" alt="<?php echo $link_alt; ?>"/><p class="menu_links"><?php echo $link_alt; ?></p></a>
+            <a href="./cart.php"><img class="icon" src="<?php echo IMG_DIR; ?>cart.png" alt="carrello"/><p class="menu_links">Carrello</p></a>
             <?php if (isset($_SESSION["email"])) {
                     $_GET["user_info"] = "user_events";
-                    echo '<a href="./user_area.php"><img src="'.IMG_DIR.'calendar.png" alt="i miei eventi"/><p class="menu_links">I miei eventi</p></a>';
-                    echo '<a href="./logout.php"><img src="'.IMG_DIR.'logout.png" alt="logout"/><p class="menu_links">Logout</p></a>';
+                    echo '<a href="./user_area.php"><img class="icon" src="'.IMG_DIR.'calendar.png" alt="i miei eventi"/><p class="menu_links">I miei eventi</p></a>';
+                    echo '<a href="./logout.php"><img class="icon" src="'.IMG_DIR.'logout.png" alt="logout"/><p class="menu_links">Logout</p></a>';
             }?>
         </nav>
     </header>
--- template/home.php
+++ template/home.php
@@ -1,7 +1,8 @@
 <section>
+    <h1>Non perderti neanche un evento che ami</h1>
     <img src="<?php echo IMG_DIR; ?>/home.jpg" alt="" />
     <form action="./search.php" method="get">
-        <label for="ricerca"><img src="<?php echo IMG_DIR; ?>/search.png" alt="ricerca" /></label>
+        <label for="ricerca"><img class="icon" src="<?php echo IMG_DIR; ?>/search.png" alt="ricerca" /></label>
         <input type="text" name="keyword" id="ricerca" />
     </form>
 </section>
