10ffd7588b27276429c36a488a5497111f6a7edc
Giorgia Rondinini <giorgia.rondinini@gmail.com>
2019-12-10 12:30:53 +0100

    Fix use of array indexes in getting profile photos

--- utils/functions.php
+++ utils/functions.php
@@ -4,6 +4,6 @@ use it\unibo\tecweb\seatheat\DatabaseHelper;
 
 function getProfileImage(DatabaseHelper $dbh, string $userEmail) {
         $userShortData = $dbh->getUsersManager()->getUserShortProfile($userEmail);
-        return $userShortData[0]["profilePhoto"]; // TODO: will need to change
+        return $userShortData["profilePhoto"]; // TODO: will need to change
     }
 ?>
