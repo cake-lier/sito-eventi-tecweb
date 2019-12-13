38ac0ea5e8ff6a633097e1d6100fbca3f003b68c
cake_lier <matteo.castellucci14@hotmail.it>
2019-12-13 12:41:31 +0100

    Change collation, add timestamp for purchases

--- db_tecweb.sql
+++ db_tecweb.sql
@@ -1,107 +1,108 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-CREATE DATABASE IF NOT EXISTS `my_seatheat` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_cs;
+CREATE DATABASE IF NOT EXISTS `my_seatheat` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `my_seatheat`;
 
 CREATE TABLE IF NOT EXISTS `administrators` (
-  `email` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `email` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `carts` (
   `eventId` int(11) NOT NULL,
   `seatId` int(11) NOT NULL,
-  `customerEmail` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `customerEmail` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `amount` int(11) NOT NULL,
   PRIMARY KEY (`eventId`,`seatId`,`customerEmail`),
   KEY `FK_CUSTOMER` (`customerEmail`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `customers` (
-  `email` varchar(30) COLLATE latin1_general_cs NOT NULL,
-  `username` varchar(30) COLLATE latin1_general_cs NOT NULL,
-  `name` varchar(30) COLLATE latin1_general_cs NOT NULL,
-  `surname` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `email` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `username` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `surname` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `birthDate` date NOT NULL,
-  `birthplace` varchar(30) COLLATE latin1_general_cs NOT NULL,
-  `currentAddress` varchar(30) COLLATE latin1_general_cs DEFAULT NULL,
-  `billingAddress` varchar(30) COLLATE latin1_general_cs NOT NULL,
-  `telephone` varchar(11) DEFAULT NULL,
+  `birthplace` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `currentAddress` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `billingAddress` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `telephone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `eventCategories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=2 ;
 
 CREATE TABLE IF NOT EXISTS `events` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) COLLATE latin1_general_cs NOT NULL,
-  `place` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `place` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `dateTime` datetime NOT NULL,
-  `description` mediumtext COLLATE latin1_general_cs NOT NULL,
-  `site` varchar(30) COLLATE latin1_general_cs DEFAULT NULL,
-  `promoterEmail` varchar(30) COLLATE latin1_general_cs DEFAULT NULL,
+  `description` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `site` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `promoterEmail` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_PROMOTER` (`promoterEmail`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `eventsToCategories` (
   `categoryId` int(11) NOT NULL,
   `eventId` int(11) NOT NULL,
   PRIMARY KEY (`categoryId`,`eventId`),
   KEY `FK_EVENT` (`eventId`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `notifications` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `message` mediumtext COLLATE latin1_general_cs NOT NULL,
+  `message` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `promoters` (
-  `email` varchar(30) COLLATE latin1_general_cs NOT NULL,
-  `organizationName` varchar(30) COLLATE latin1_general_cs NOT NULL,
-  `VATid` char(11) NOT NULL,
-  `website` varchar(30) COLLATE latin1_general_cs DEFAULT NULL,
+  `email` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `organizationName` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `VATid` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `website` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`email`),
   UNIQUE KEY `ID_PROMOTER` (`VATid`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `purchases` (
   `eventId` int(11) NOT NULL,
   `seatId` int(11) NOT NULL,
-  `customerEmail` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `customerEmail` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `amount` int(11) NOT NULL,
-  PRIMARY KEY (`eventId`,`seatId`,`customerEmail`),
+  `dateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`eventId`,`seatId`,`customerEmail`,`dateTime`),
   KEY `FK_CUSTOMER` (`customerEmail`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `seatCategories` (
   `eventId` int(11) NOT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `price` decimal(13,2) NOT NULL,
   `seats` int(11) NOT NULL,
   PRIMARY KEY (`eventId`,`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs AUTO_INCREMENT=1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `email` varchar(30) COLLATE latin1_general_cs NOT NULL,
-  `password` varchar(255) COLLATE latin1_general_cs NOT NULL,
-  `profilePhoto` mediumblob NOT NULL,
-  `type` enum('c','p','a') COLLATE latin1_general_cs NOT NULL,
+  `email` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `profilePhoto` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `type` enum('c','p','a') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `usersNotifications` (
   `notificationId` int(11) NOT NULL,
-  `email` varchar(30) COLLATE latin1_general_cs NOT NULL,
+  `email` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `dateTime` datetime NOT NULL,
   `visualized` tinyint(1) NOT NULL,
   PRIMARY KEY (`notificationId`,`email`,`dateTime`),
   KEY `FK_USER` (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
