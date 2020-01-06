d1d7429a449f868e3307572350970ad4c2147292
Giorgia Rondinini <giorgia.rondinini@gmail.com>
2020-01-06 23:50:32 +0100

    Nicer text in info sections

--- template/info_section.php
+++ template/info_section.php
@@ -4,12 +4,15 @@ $title = "";
 if (isset($_GET["type"])) {
     switch ($_GET["type"]) {
         case "chi_siamo":
-            $text = "SeatHeat é un sito per la vendita di biglietti di eventi. Icone da <a href=\"https://www.streamlineicons.com/\">Streamline</a>.
-            Immagine della home di Antoine Delanoix.";
+            $text = "SeatHeat é un sito per la vendita di biglietti di eventi."
+                    ."Ringraziamo <a href=\"https://www.streamlineicons.com/\">Streamline</a> per le icone, "
+                    ."e Antoine Delanoix per l'immagine nella home.";
             $title = "Chi siamo";
         break;
         case "contatti":
-            $text = "Per contattarci, manda una mail a seatheat@seatheat.it";
+            $text = "Per contattarci, manda una mail a seatheat@seatheat.it, "
+                    ."oppure invia un messaggio agli admin tramite l'apposita "
+                    ."sezione!";
             $title = "Contatti";
         break;
         case "termini":
@@ -17,8 +20,11 @@ if (isset($_GET["type"])) {
             $title = "Termini di servizio";
         break;
         case "privacy":
-            $text = "Non ci frega nulla dei vostri dati. Li teniamo sul database (non in Europa). Li terremo finché sarete nostri 
-                     clienti. Gli organizzatori possono vederli. Fine. Usiamo solo cookie tecnici.";
+            $text = "Il sito usa solo cookie tecnici al fine di fornire i nostri servizi nel miglior modo possibile. "
+                    ."I dati personali che trattiamo sono unicamente quelli richiesti in fase di registrazione, é "
+                    ."possibile trovarne un riepilogo nella propria area personale. I dati sono mantenuti nel nostro "
+                    ."database, non necessarimente in territorio europeo, fino alla chiusura dell'account, effettuabile "
+                    ."dall'apposita sezione nell'area personale. ";
             $title = "Informativa privacy";
         break;
         default:
