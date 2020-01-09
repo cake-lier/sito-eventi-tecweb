237fcf000bd30302576463f36d2b17f81f1387e9
cake_lier <matteo.castellucci14@hotmail.it>
2020-01-09 14:19:44 +0100

    Add events count in search for tags

--- get_events_count.php
+++ get_events_count.php
@@ -15,17 +15,35 @@ try {
                 }
             });
         }
-        $data["count"] = $type === 1
-                         ? $dbh->getEventsManager()->getEventsCount(isset($_GET["keyword"]) ? $_GET["keyword"] : "",
-                                                                    isset($_GET["free"]) ? $_GET["free"] : true,
-                                                                    isset($_GET["place"]) ? $_GET["place"] : "",
-                                                                    isset($_GET["date"]) ? $_GET["date"] : "",
-                                                                    $promoterEmail)
-                         : ($type === 2
-                            ? $dbh->getEventsManager()->getPurchasedEventsCount()
-                            : ($type === 3
-                               ? $dbh->getEventsManager()->getCreatedEventsCount()
-                               : 0));
+        if (isset($_GET["tags"]) && $_GET["tags"] !== "") {
+            $eventIdsUncategorized = $dbh->getEventsManager()->getEventIdsFiltered(-1,
+                                                                                   -1,
+                                                                                   isset($_GET["keyword"]) ? $_GET["keyword"] : "",
+                                                                                   isset($_GET["free"]) ? $_GET["free"] : true,
+                                                                                   isset($_GET["place"]) ? $_GET["place"] : "",
+                                                                                   isset($_GET["date"]) ? $_GET["date"] : "",
+                                                                                   $promoterEmail);
+            $tags = explode(" ", str_replace("#", "", $_GET["tags"]));
+            $filteredIds = array();
+            array_walk($eventIdsUncategorized, function($id) use ($dbh, $tags, &$filteredIds) {
+                if ($dbh->getEventsManager()->hasEventCategories($id, ...$tags)) {
+                    array_push($filteredIds, $id);
+                }
+            });
+            $data["count"] = count($filteredIds);
+        } else {
+            $data["count"] = $type === 1
+                             ? $dbh->getEventsManager()->getEventsCount(isset($_GET["keyword"]) ? $_GET["keyword"] : "",
+                                                                        isset($_GET["free"]) ? $_GET["free"] : true,
+                                                                        isset($_GET["place"]) ? $_GET["place"] : "",
+                                                                        isset($_GET["date"]) ? $_GET["date"] : "",
+                                                                        $promoterEmail)
+                             : ($type === 2
+                                ? $dbh->getEventsManager()->getPurchasedEventsCount()
+                                : ($type === 3
+                                   ? $dbh->getEventsManager()->getCreatedEventsCount()
+                                   : 0));
+        }
         $data["result"] = true;
     }
 } catch (\Exception $e) {
